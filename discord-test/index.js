1bff4d647eca654d70ff97e4a7b874b72b444235
NimaLecter <79058123+NimaLecter@users.noreply.github.com>
2021-07-19 10:35:29 +0430

    Discord_Test

---
+++ discord-test/index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login('NzY3MDUwMjM3NjgwMTU2Njky.X4sRMw.x5t2ClwqIDok9oQpLtM9uqScfGk');
